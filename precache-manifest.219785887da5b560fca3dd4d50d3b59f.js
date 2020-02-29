f076fe26ff905d5c1dd6e93e696928e96982da37
dave <davidreact@gmail.com>
2020-02-29 11:54:17 -0500

    Updates

---
+++ precache-manifest.219785887da5b560fca3dd4d50d3b59f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4860f40f1ef7102a7ba5199fd024f927",
+    "url": "/dave-kinghorn-me/index.html"
+  },
+  {
+    "revision": "2632511c95a3858593a2",
+    "url": "/dave-kinghorn-me/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "27436ca24431a4cc3ee9",
+    "url": "/dave-kinghorn-me/static/css/main.1eb3b466.chunk.css"
+  },
+  {
+    "revision": "2632511c95a3858593a2",
+    "url": "/dave-kinghorn-me/static/js/2.f6d7e814.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/dave-kinghorn-me/static/js/2.f6d7e814.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27436ca24431a4cc3ee9",
+    "url": "/dave-kinghorn-me/static/js/main.3aa70930.chunk.js"
+  },
+  {
+    "revision": "e0fd9e62e63d464c75d9",
+    "url": "/dave-kinghorn-me/static/js/runtime-main.09a12f7d.js"
+  }
+]);
