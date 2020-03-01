eef434382a82df8bf2a1e703e432b0b79521d395
dave <davidreact@gmail.com>
2020-03-01 18:22:09 -0500

    Updates

---
+++ precache-manifest.740ba0a7bf6b6b81a46f073071170a78.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "451fbc7ef732bfc958968112d482ff19",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e09ebd08791c01dc044b",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "09009e03e20dd39663e0",
+    "url": "/static/css/main.19e44e13.chunk.css"
+  },
+  {
+    "revision": "e09ebd08791c01dc044b",
+    "url": "/static/js/2.f391dd96.chunk.js"
+  },
+  {
+    "revision": "df8be702f0cc9d299c0ff42ca03d3e40",
+    "url": "/static/js/2.f391dd96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09009e03e20dd39663e0",
+    "url": "/static/js/main.d5bfcd37.chunk.js"
+  },
+  {
+    "revision": "a2f8992ce53c4ed43d06",
+    "url": "/static/js/runtime-main.3c588987.js"
+  }
+]);
