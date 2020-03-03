59f500a5caa976c9ade8ef8d796b9cc43d59cfca
dave <davidreact@gmail.com>
2020-03-02 21:04:49 -0500

    Added link to github.

--- src/NavBar.js
+++ src/NavBar.js
@@ -11,6 +11,7 @@ const NavBar = () => (
             {/*<Nav.Link href={process.env.PUBLIC_URL + "/WorkHistory"}>Work History</Nav.Link>*/}
             {/*<Nav.Link href={process.env.PUBLIC_URL + "/Projects"}>Projects</Nav.Link>*/}
             {/*<Nav.Link href={process.env.PUBLIC_URL + "/Publications"}>Publications</Nav.Link>*/}
+            <Nav.Link href={'github.com/Davidjwkinghorn'}>GitHub</Nav.Link>
             <Nav.Link href={process.env.PUBLIC_URL + '/assets/documents/Dave_Whitman-Kinghorn_Resume.pdf'}>Resume</Nav.Link>
             {/*<Nav.Link href={process.env.PUBLIC_URL + "/ContactMe"}>Contact Me</Nav.Link>*/}
         </Nav>
