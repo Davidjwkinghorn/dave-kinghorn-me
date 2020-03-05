eb77a2266f6614a7449aa2afd0449419c5d883c6
dave <davidreact@gmail.com>
2020-03-05 11:32:08 -0500

    Fixed typo.

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,46 +4,53 @@ import './HomePage.css';
 
 const HomePage = () => (
     <>
-        <div className="home-carousel">
-            <Carousel>
-                <Carousel.Item className="carousel-item">
-                    <img
-                        className="d-block w-100"
-                        id="carousel-image"
-                        src={process.env.PUBLIC_URL + '/assets/images/london.jpg'}
-                        alt="London Cityscape at night."
-                    />
-                    <Carousel.Caption>
-                        <h3>Born</h3>
-                        <p>in England.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        id="carousel-image"
-                        src={process.env.PUBLIC_URL + '/assets/images/lexington1.jpg'}
-                        alt="Lexington Massachusetts Depot Square."
-                    />
-                    <Carousel.Caption>
-                        <h3>Raised</h3>
-                        <p>in Lexington Massachusetts.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        id="carousel-image"
-                        src={process.env.PUBLIC_URL + '/assets/images/rit.jpg'}
-                        alt="Rochester Institute of Technology"
-                    />
-                    <Carousel.Caption className="carousel-caption">
-                        <h3>Studying</h3>
-                        <p>Software Egnineering at the Rochester Institute of Technology.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-            </Carousel>
-        </div>
+        <Carousel>
+            <Carousel.Item className="carousel-item">
+                <img
+                    className="d-block w-100"
+                    id="carousel-image"
+                    src={process.env.PUBLIC_URL + '/assets/images/london.jpg'}
+                    alt="London Cityscape at night."
+                />
+                <Carousel.Caption>
+                    <h3 className="carousel-caption-h3">Born</h3>
+                    <p className="carousel-caption-p">in England.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    id="carousel-image"
+                    src={process.env.PUBLIC_URL + '/assets/images/lexington1.jpg'}
+                    alt="Lexington Massachusetts Depot Square."
+                />
+                <Carousel.Caption>
+                    <h3 className="carousel-caption-h3">Raised</h3>
+                    <p className="carousel-caption-p">in Lexington Massachusetts.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    id="carousel-image"
+                    src={process.env.PUBLIC_URL + '/assets/images/rit.jpg'}
+                    alt="Rochester Institute of Technology"
+                />
+                <Carousel.Caption className="carousel-caption">
+                    <h3 className="carousel-caption-h3">Studying</h3>
+                    <p className="carousel-caption-p">Software Engineering at the Rochester Institute of Technology.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+        </Carousel>
+        {/*<div>*/}
+        {/*    <p>"All that is gold does not glitter,<br/>*/}
+        {/*    Not all those who wander are lost"<br/>*/}
+        {/*        - J.R.R. Tolkien</p>*/}
+        {/*</div>*/}
+        {/*<div>*/}
+        {/*    <h2>Other text</h2>*/}
+        {/*    <p>Paragraph</p>*/}
+        {/*</div>*/}
     </>
 );
 
