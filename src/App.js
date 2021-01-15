62e5b11789a7febdeba1dab1c7c52ef94f08e2dd
Davidjwkinghorn <davidreact@gmail.com>
2021-01-15 17:51:34 -0500

    Created footer outline.

--- src/App.js
+++ src/App.js
@@ -7,9 +7,10 @@ import ContactMe from "./pages/ContactMe";
 import Projects from "./pages/Projects";
 import Publications from "./pages/Publications";
 import WorkHistory from "./pages/WorkHistory";
+
 import {ParallaxProvider} from "react-scroll-parallax";
 import { IconContext } from "react-icons";
-
+import { Row, Col, Container } from 'react-bootstrap';
 
 export default class App extends Component {
     render() {
@@ -18,12 +19,34 @@ export default class App extends Component {
                 <IconContext.Provider value={{ color: "grey", className: "global-icon" }}>
                     <Router>
                         <div className="App">
-                            <NavBar/>
-                            <Route exact path={"/"} component={HomePage}/>
-                            <Route path={"/ContactMe"} component={ContactMe}/>
-                            <Route path={"/Projects"} component={Projects}/>
-                            <Route path={"/Publications"} component={Publications}/>
-                            <Route path={"/WorkHistory"} component={WorkHistory}/>
+                            <div><NavBar/></div>
+                            <div>
+                                <Route exact path={"/"} component={HomePage}/>
+                                <Route path={"/ContactMe"} component={ContactMe}/>
+                                <Route path={"/Projects"} component={Projects}/>
+                                <Route path={"/Publications"} component={Publications}/>
+                                <Route path={"/WorkHistory"} component={WorkHistory}/>
+                            </div>
+                            <div class="d-flex flex-column">
+                                <footer class="footer">
+                                    <Container>
+                                        <Row>
+                                            <Col>
+                                                <p>Col 1</p>
+                                            </Col>
+                                            <Col>
+                                                <p>Col 1</p>
+                                            </Col>
+                                            <Col>
+                                                <p>Col 1</p>
+                                            </Col>
+                                            <Col>
+                                                <p>Col 1</p>
+                                            </Col>
+                                        </Row>
+                                    </Container>
+                                </footer>
+                            </div>
                         </div>
                     </Router>
                 </IconContext.Provider>
