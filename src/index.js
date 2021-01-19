562fe65601049327f926903f32e58964f354de12
Davidjwkinghorn <davidreact@gmail.com>
2021-01-18 20:44:23 -0500

    Styling fixes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.scss';
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App style="background-color: $darkgrey ;"/>, document.getElementById('root'));
