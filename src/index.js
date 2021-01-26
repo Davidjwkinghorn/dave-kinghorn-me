7d852773c51ccf03af93932f9561f8db11323eab
Davidjwkinghorn <davidreact@gmail.com>
2021-01-26 15:53:24 -0500

    Removed no longer used import.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.scss';
-import { Background } from 'react-parallax';
 
 
 ReactDOM.render(<App/>, document.getElementById('root'));
