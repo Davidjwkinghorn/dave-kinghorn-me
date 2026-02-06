5f257d85c583996f3efb5ac0ca7fd347f3327e9e
Dave Kinghorn <davidreact@gmail.com>
2026-02-06 15:41:18 -0500

    Updated and upgraded various dependencies. Resolved sass issues on courses page.

--- src/pages/ContactMe.js
+++ src/pages/ContactMe.js
@@ -4,6 +4,7 @@ export default class ContectMe extends React.Component {
   render() {
     return (
       <>
+        {/* placeholder for contact me page, will be updated in the future */}
         <h1>Yell really really loud and I'll hear.</h1>
       </>
     );
